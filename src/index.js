94067d2cc6bd64c2f8c61b6ebfe5a62812d3b09c
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-21 23:52:08 +0100

    add function to fetch data from api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-const hello = () => console.log('hello');
 
-const array = ['a', 'b', 'c'];
 
-const spread = [...array];
+function printCityData(cityData) {
+  console.log(cityData);
+  if ('location' in cityData) {
+    console.log('getting location');
+  } else if ('error' in cityData) {
+    console.log('getting error');
+  }
+}
 
-console.log(spread);
+function fetchCityWeatherData(cityName = 'Lagos') {
+    const sampleURL = `./data/forecast-data.json`;
+    // const actualURL = `https://api.weatherapi.com/v1/forecast.json?key=871ceab34975437482c100138230112&q=${cityName}&days=3&api=yes`
+//   fetch(
+//     actualURL,
+//     { mode: 'cors' }
+//   )
+    fetch(sampleURL)
+    .then((response) => response.json())
+    .then((response) => {
+      printCityData(response);
+      console.log(cityName);
+    });
+}
 
-hello();
+fetchCityWeatherData();
+
+// printCityData();
