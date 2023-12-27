d576f63ca4cc53b2a194b74e1f1fe2043b165ee2
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-27 03:55:35 +0100

    modify fetch request to use weather api

--- src/index.js
+++ src/index.js
@@ -107,10 +107,10 @@ function printCityData(cityData) {
 }
 
 function fetchCityWeatherData(cityName = 'Lagos') {
-  const sampleURL = `./data/forecast-data.json`;
-  // const actualURL = `https://api.weatherapi.com/v1/forecast.json?key=871ceab34975437482c100138230112&q=${cityName}&days=3&api=yes`;
-  // fetch(actualURL, { mode: 'cors' })
-    fetch(sampleURL)
+  // const sampleURL = `./data/forecast-data.json`;
+  const actualURL = `https://api.weatherapi.com/v1/forecast.json?key=871ceab34975437482c100138230112&q=${cityName}&days=3&api=yes`;
+  fetch(actualURL, { mode: 'cors' })
+    // fetch(sampleURL)
     .then((response) => response.json())
     .then((response) => {
       printCityData(response);
