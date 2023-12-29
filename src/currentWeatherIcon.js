d5be64700bf9f3fc611801dd38cb6ca4c2909269
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-29 14:57:25 +0100

    export images to be displayed based on weather condition

--- src/currentWeatherIcon.js
+++ src/currentWeatherIcon.js
@@ -1,10 +1,199 @@
 import img113Day from './icons/day/113.png';
 import img113Night from './icons/night/113.png';
+import img116Day from './icons/day/116.png';
+import img116Night from './icons/night/116.png';
+import img119Day from './icons/day/119.png';
+import img119Night from './icons/night/119.png';
+import img122Day from './icons/day/122.png';
+import img122Night from './icons/night/122.png';
+import img143Day from './icons/day/143.png';
+import img143Night from './icons/night/143.png';
+import img176Day from './icons/day/176.png';
+import img176Night from './icons/night/176.png';
+import img179Day from './icons/day/179.png';
+import img179Night from './icons/night/179.png';
+import img182Day from './icons/day/182.png';
+import img182Night from './icons/night/182.png';
+import img185Day from './icons/day/185.png';
+import img185Night from './icons/night/185.png';
+import img200Day from './icons/day/200.png';
+import img200Night from './icons/night/200.png';
+import img227Day from './icons/day/227.png';
+import img227Night from './icons/night/227.png';
+import img230Day from './icons/day/230.png';
+import img230Night from './icons/night/230.png';
+import img248Day from './icons/day/248.png';
+import img248Night from './icons/night/248.png';
+import img260Day from './icons/day/260.png';
+import img260Night from './icons/night/260.png';
+import img263Day from './icons/day/263.png';
+import img263Night from './icons/night/263.png';
+import img266Day from './icons/day/266.png';
+import img266Night from './icons/night/266.png';
+import img281Day from './icons/day/281.png';
+import img281Night from './icons/night/281.png';
+import img284Day from './icons/day/284.png';
+import img284Night from './icons/night/284.png';
+import img293Day from './icons/day/293.png';
+import img293Night from './icons/night/293.png';
+import img296Day from './icons/day/296.png';
+import img296Night from './icons/night/296.png';
+import img299Day from './icons/day/299.png';
+import img299Night from './icons/night/299.png';
+import img302Day from './icons/day/302.png';
+import img302Night from './icons/night/302.png';
+import img305Day from './icons/day/305.png';
+import img305Night from './icons/night/305.png';
+import img308Day from './icons/day/308.png';
+import img308Night from './icons/night/308.png';
+import img311Day from './icons/day/311.png';
+import img311Night from './icons/night/311.png';
+import img314Day from './icons/day/314.png';
+import img314Night from './icons/night/314.png';
+import img317Day from './icons/day/317.png';
+import img317Night from './icons/night/317.png';
+import img320Day from './icons/day/320.png';
+import img320Night from './icons/night/320.png';
+import img323Day from './icons/day/323.png';
+import img323Night from './icons/night/323.png';
+import img326Day from './icons/day/326.png';
+import img326Night from './icons/night/326.png';
+import img329Day from './icons/day/329.png';
+import img329Night from './icons/night/329.png';
+import img332Day from './icons/day/332.png';
+import img332Night from './icons/night/332.png';
+import img335Day from './icons/day/335.png';
+import img335Night from './icons/night/335.png';
+import img338Day from './icons/day/338.png';
+import img338Night from './icons/night/338.png';
+import img350Day from './icons/day/350.png';
+import img350Night from './icons/night/350.png';
+import img353Day from './icons/day/353.png';
+import img353Night from './icons/night/353.png';
+import img356Day from './icons/day/356.png';
+import img356Night from './icons/night/356.png';
+import img359Day from './icons/day/359.png';
+import img359Night from './icons/night/359.png';
+import img362Day from './icons/day/362.png';
+import img362Night from './icons/night/362.png';
+import img365Day from './icons/day/365.png';
+import img365Night from './icons/night/365.png';
+import img368Day from './icons/day/368.png';
+import img368Night from './icons/night/368.png';
+import img371Day from './icons/day/371.png';
+import img371Night from './icons/night/371.png';
+import img374Day from './icons/day/374.png';
+import img374Night from './icons/night/374.png';
+import img377Day from './icons/day/377.png';
+import img377Night from './icons/night/377.png';
+import img386Day from './icons/day/386.png';
+import img386Night from './icons/night/386.png';
+import img389Day from './icons/day/389.png';
+import img389Night from './icons/night/389.png';
+import img392Day from './icons/day/392.png';
+import img392Night from './icons/night/392.png';
+import img395Day from './icons/day/395.png';
+import img395Night from './icons/night/395.png';
+
 import createImg from './createImg';
 
 const images = {
   '113Day': img113Day,
   '113Night': img113Night,
+  '116Day': img116Day,
+  '116Night': img116Night,
+  '119Day': img119Day,
+  '119Night': img119Night,
+  '122Day': img122Day,
+  '122Night': img122Night,
+  '143Day': img143Day,
+  '143Night': img143Night,
+  '176Day': img176Day,
+  '176Night': img176Night,
+  '179Day': img179Day,
+  '179Night': img179Night,
+  '182Day': img182Day,
+  '182Night': img182Night,
+  '185Day': img185Day,
+  '185Night': img185Night,
+  '200Day': img200Day,
+  '200Night': img200Night,
+  '227Day': img227Day,
+  '227Night': img227Night,
+  '230Day': img230Day,
+  '230Night': img230Night,
+  '248Day': img248Day,
+  '248Night': img248Night,
+  '260Day': img260Day,
+  '260Night': img260Night,
+  '263Day': img263Day,
+  '263Night': img263Night,
+  '266Day': img266Day,
+  '266Night': img266Night,
+  '281Day': img281Day,
+  '281Night': img281Night,
+  '284Day': img284Day,
+  '284Night': img284Night,
+  '293Day': img293Day,
+  '293Night': img293Night,
+  '296Day': img296Day,
+  '296Night': img296Night,
+  '299Day': img299Day,
+  '299Night': img299Night,
+  '302Day': img302Day,
+  '302Night': img302Night,
+  '305Day': img305Day,
+  '305Night': img305Night,
+  '308Day': img308Day,
+  '308Night': img308Night,
+  '311Day': img311Day,
+  '311Night': img311Night,
+  '314Day': img314Day,
+  '314Night': img314Night,
+  '317Day': img317Day,
+  '317Night': img317Night,
+  '320Day': img320Day,
+  '320Night': img320Night,
+  '323Day': img323Day,
+  '323Night': img323Night,
+  '326Day': img326Day,
+  '326Night': img326Night,
+  '329Day': img329Day,
+  '329Night': img329Night,
+  '332Day': img332Day,
+  '332Night': img332Night,
+  '335Day': img335Day,
+  '335Night': img335Night,
+  '338Day': img338Day,
+  '338Night': img338Night,
+  '350Day': img350Day,
+  '350Night': img350Night,
+  '353Day': img353Day,
+  '353Night': img353Night,
+  '356Day': img356Day,
+  '356Night': img356Night,
+  '359Day': img359Day,
+  '359Night': img359Night,
+  '362Day': img362Day,
+  '362Night': img362Night,
+  '365Day': img365Day,
+  '365Night': img365Night,
+  '368Day': img368Day,
+  '368Night': img368Night,
+  '371Day': img371Day,
+  '371Night': img371Night,
+  '374Day': img374Day,
+  '374Night': img374Night,
+  '377Day': img377Day,
+  '377Night': img377Night,
+  '386Day': img386Day,
+  '386Night': img386Night,
+  '389Day': img389Day,
+  '389Night': img389Night,
+  '392Day': img392Day,
+  '392Night': img392Night,
+  '395Day': img395Day,
+  '395Night': img395Night,
 };
 
 export default function getImg(imgCode) {
